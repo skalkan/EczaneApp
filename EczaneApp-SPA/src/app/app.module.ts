700d4a9193e1f1b11873dd7c975fb90491014fb3
Serdar Kalkan <skalkan@gmail.com>
2019-01-25 21:26:16 +0300

    member-edit component created

--- EczaneApp-SPA/src/app/app.module.ts
+++ EczaneApp-SPA/src/app/app.module.ts
@@ -25,6 +25,7 @@ import { MemberCardComponent } from './members/member-card/member-card.component
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 import { MemberDetailResolver } from './_resolvers/member-detail.resolver';
 import { MemberListResolver } from './_resolvers/member-list.resolver';
+import { MemberEditComponent } from './members/member-edit/member-edit.component';
 
 export function tokenGetter() {
   return localStorage.getItem('token');
@@ -41,7 +42,8 @@ export function tokenGetter() {
     ListsComponent,
     MessagesComponent,
     MemberCardComponent,
-    MemberDetailComponent
+    MemberDetailComponent,
+    MemberEditComponent
   ],
   imports: [
     BrowserModule,
